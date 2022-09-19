c5f29b312844d6bf53a3b5837598136c7e755288
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-19 14:08:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT instructor_id, instructor_name from instructor";
+$sql = "SELECT course_id, prefix, number, description from course";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
