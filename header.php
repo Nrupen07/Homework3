ad56676ca427059a2a53d0dc1c04b5d70ce06965
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-10-05 14:03:39 -0500

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Homework 3</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,6 +15,17 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="course.php">Classes</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="course-section.php">Section Number</a>
+        </li> <li class="nav-item">
+          <a class="nav-link" href="instructor-cards.php">Name Cards</a>
+        </li> <li class="nav-item">
+          <a class="nav-link" href="instructor-section.php">Section Instructors</a>
+        </li> <li class="nav-item">
+          <a class="nav-link" href="instructors.php">Instructors</a>
+        </li> <li class="nav-item">
+          <a class="nav-link" href="section.php">Sections</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
